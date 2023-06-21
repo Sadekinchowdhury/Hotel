709713e24c2230bc3e1748896bb557d24c846316
sadekinchowdhury <rumelchow340@gmail.com>
2023-06-21 21:22:05 +0600

    spinner

--- app/page.js
+++ app/page.js
@@ -17,12 +17,12 @@ export default function Home() {
 
   return (
     <div className=" h-screen">
-      {isLoading ? <>
-        <div className="flex items-center justify-center h-screen">
-          <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-16 w-16"></div>
-        </div></> : <>
-        <Main />
-      </>}
+      {isLoading ?
+        <div class="flex justify-center items-center h-full">
+          <img class="h-16 w-16" src="https://icons8.com/preloaders/preloaders/1488/Iphone-spinner-2.gif" alt="" />
+        </div> : <>
+          <Main />
+        </>}
 
 
     </div>
